dd652a7cc745fdc5e6a8a5a2470636fb93c5dd1e
Amarachi Victoria <amarachivickyamarachi@gmail.com>
2025-07-20 13:13:25 +0100

    Add files via upload

---
+++ JS/CyberSearch.js
@@ -0,0 +1,19 @@
+ setInterval(function() {
+    // const alertInfo = document.createElement('div');
+    // alertInfo.setAttribute('class', 'alert')
+    // alertInfo.textContent = 'Malware is a malicious software sent to attack your computer by an attacker'
+
+    // const alertButton = document.createElement('button');
+    // alertButton.setAttribute('class', 'btn btn-danger');
+    // alertButton.textContent = 'Close';
+    // alertButton.addEventListener('click', function() {
+    //     alertInfo.remove();
+    // });
+    // alertInfo.appendChild(alertButton);
+
+    // const alertContainer = document.querySelector('.container');
+    // alertContainer.appendChild(alertInfo);
+  alert('social Engineering is a type of cyber attack where the attacker persuade people into revealing confidential information')
+}, 600000 );
+
+
